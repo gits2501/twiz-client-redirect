9012ba39e716a2cb2f03fcb097569797620148af
gits2501 <gits2501@users.noreply.github.com>
2018-06-21 09:42:44 +0200

    Added code coverage

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,13 +1,12 @@
-var CustomError = require('twiz-client-utils').CustomError;
+var CustomError     = require('twiz-client-utils').CustomError;
+var throwAsyncError = require('twiz-client-utils').throwAsyncError;
    
    function Redirect (args){     // used to redirect user to twitter interstitals page (authorize leg)
     
-      this.newWindow     = args.newWindow;
-      this.url           = args.redirectionUrl;
-      this.callback_func = args.callback_func;
-    
-      // OAuth.call(this);
-
+      this.newWindow     = args.newWindow;      // new tap / popup features
+      this.url           = args.redirectionUrl; // url whre twitter will direct user after authorization
+      this.callback_func = args.callback_func;  // callback if there is no promise
+      this.reject        = args.reject
       this.requestToken;    // data from request token step   
       
       CustomError.call(this); // add CustomError feature
@@ -15,108 +14,114 @@ var CustomError = require('twiz-client-utils').CustomError;
          noCallbackFunc: 'You must specify a callback function',
          callbackURLnotConfirmed: "Redirection(callback) url you specified wasn't confirmed by Twitter"
       })
+
+    
    }
 
-   // Redirect.prototype = Object.create(OAuth.prototype);
-  
 
-   Redirect.prototype.redirection = function(resolve, error, sentData){ // Callback function for 2nd step
-                                                  
-      console.log("From twitter request_token: ", sentData);
-      console.log('sentData type: ',typeof sentData);
-      console.log('error :', error);
-
-      if(error || !sentData.oauth_token){ // on error or on valid data deliver it to user 
+   Redirect.prototype.redirection = function(resolve, res){ // Callback function for 2nd step
+      //  console.log(res);
+      //console.log("From twitter request_token: ", res.data);
+      //console.log('res.data type: ',typeof res.data);
       
-         this.deliverData(resolve, {'error': error, 'data': sentData});
+      this.res = res;                        // save response reference
+      //  console.log('|redirection res|:', res.data || 'no data')
+      if(res.error || !res.data.oauth_token){ // on response error or on valid data deliver it to user 
+         this.deliverData(resolve, res);
          return;
       }
- 
-      this.requestToken = sentData;      // set requestToken data
-      this.confirmCallback(sentData);    // confirm that twitter accepted user's redirection(callback) url
-      this.saveRequestToken(window.localStorage, sentData.oauth_token); 
+      this.requestToken = res.data;      // set requestToken data
+      this.confirmCallback(res.data);    // confirm that twitter accepted user's redirection(callback) url
+      this.saveRequestToken(window.localStorage, res.data.oauth_token); // save token for url authorization 
       this.redirect(resolve)             // redirect user to twitter for authorization 
    };
    
-   Redirect.prototype.deliverData = function(resolve, obj){ // delivers data to user by promise or
-                                                                        // by callback function
-      console.log('in deliverData, obj:', obj);
-      if(resolve){ console.log('has promise')
-         resolve(obj);
+   Redirect.prototype.deliverData = function(resolve, res){ // delivers data to user by promise or
+                                                            // by callback function
+      if(resolve){                                  // console.log('has promise')
+         resolve(res);
          return;
       }
-      
+
       if(this.callback_func) {             // when no promise is avalable invoke callback
-         this.callback_func(obj);
+         this.callback_func(res);
          return;
       }
-                                       
-      throw this.CustomError('noCallbackFunc'); // raise error when there is no promise or callback present                      
+   
+      this.throwAsyncError(this.CustomError('noCallbackFunc')); // raise error when there is no promise or
+                                                                // callback present
    }
+  
+   Redirect.prototype.throwAsyncError = throwAsyncError;        // promise (async) aware error throwing
 
    Redirect.prototype.confirmCallback = function (sent){ // makes sure that twitter is ok with redirection url
-      if(sent.oauth_callback_confirmed !== "true") throw this.CustomError('callbackURLnotConfirmed');
+      // console.log('confirmed: +++ ',sent.oauth_callback_confirmed)
+      if(sent.oauth_callback_confirmed !== "true")
+         this.throwAsyncError(this.CustomError('callbackURLnotConfirmed'));
    }
  
    Redirect.prototype.saveRequestToken = function(storage, token){ // save token to storage
       storage.requestToken_ = null;                                // erase any previous tokens, note null is
                                                                    // actualy transformed to string "null"
-               
       storage.requestToken_ =  token;                              // save token to storage
-      console.log('storage before: ', storage); 
+      //console.log('storage before: ', storage); 
    }
 
    Redirect.prototype.redirect = function(resolve){ // redirects user to twitter for authorization   
-      console.log('RESOLVE : ', resolve);
-      var url = this.url + "?" + 'oauth_token=' + this.requestToken.oauth_token; 
-                                                                                 // assemble url for second leg
+      //console.log('RESOLVE : ', resolve);
+  
+      var url = this.url + "?" + 'oauth_token=' + this.requestToken.oauth_token; // assemble url for second leg
+      this.adjustResponse(this.res);                               // removes this.res.data                                                                               
 
       if(!this.newWindow){ // single page app
          this.SPA(resolve, url); // redirects current window to url
-         return
+         return;
 
       }
                          
-      this.site(resolve, url); // site
+      this.site(resolve, url); // site 
       
    };
+   
+   Redirect.prototype.adjustResponse = function(res){ // tunes response so id doesnt have data when redirecting
+      res.data = '';    // never send (request token) data to user 
+   }
 
    Redirect.prototype.SPA = function(resolve, url){   // logic for Single Page Apps
+      function redirectCurrentWindow(){ window.location = url; }// redirects window we are currently in (no popUp)
+      
+      this.res.redirection = true;  // since there is no newWindow reference indicate that redirection happens
      
-      function redirectCurrentWindow(){ window.location = url }// redirects window we are currently in (no popUp)
-      var obj = { 'redirection': true } // since there is no newWindow reference, just indicate redirection
-     
-      if(resolve){                 // promise
-         resolve(obj);                                 // resolve with obj
-         Promise.resolve()             
-         .then(function(){ redirectCurrentWindow() })  // redirect asap
-         return
-      }
+      if(resolve){                   // resolve promise
+         resolve(this.res);          // resolve with response object
 
-      if(this.callback_func){     // when no promise call user callback func
-         this.callback_func(obj);                                 // run callback with token
-         setTimeout(function(){redirectCurrentWindow()}, 2000) ;  // redirect asap
+         Promise.resolve()             
+         .then(function(){           // redirect asap
+            redirectCurrentWindow();
+         })
          return;
       }
 
-      
-      throw this.CustomError('noCallbackFunc'); // raise error when there is no promise or callback present
+      if(this.callback_func){                                    // when no promise call user callback func
+         this.callback_func(this.res);                           // run callback with token
+         setTimeout(function(){ redirectCurrentWindow() }, 0) ;  // redirect asap
+         return; 
+      }
+
+      this.throwAsyncError(this.CustomError('noCallbackFunc')); // raise error when there is no promise or callback present
    }
 
-   Redirect.prototype.site = function(resolve, url){
-
-      var opened = this.openWindow();       // open new window and save its reference
+   Redirect.prototype.site = function(resolve, url){ 
+      var opened = this.openWindow();       // open new window/popup and save its reference
       opened.location = url;                // change location (redirect)
-       
-      this.deliverData(resolve, { 'window': opened })       // newWindow reference
+      
+      this.res.window = opened;             // newWindow reference
+      this.deliverData(resolve, this.res);      
    
    }
 
-   Redirect.prototype.openWindow = function(){ // opens pop-up and puts in under current window
-      console.log("==== POP-UP =====");
+   Redirect.prototype.openWindow = function(){ // opens pop-up and puts in current window reference
       this.newWindow.window = window.open('', this.newWindow.name, this.newWindow.features);
-      console.log("this.newWindow: ", this.newWindow.window ); 
-
       return this.newWindow.window;
    }
 
--- test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-                                
-Xvfb :2 -screen 5 1024x768x8 &   # Start virtual framebuffer display
-export DISPLAY=:1.5
-
-npm run test-browser;            # Test browser code 
